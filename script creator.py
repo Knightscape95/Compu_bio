f2b43d0f05d14f95011ec03f122dc1ab78a02f93
Harshank-Matkar <138029863+Knightscape95@users.noreply.github.com>
2024-01-14 23:36:19 +0530

    Update script creator.py

--- script creator.py
+++ script creator.py
@@ -1,26 +1,16 @@
-import requests
-from bs4 import BeautifulSoup
-import re
 import tensorflow as tf
+import tensorflow_datasets as tfds
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Embedding, LSTM, Dense, Attention
+from tensorflow.keras.layers import Input, Dense, Embedding, PositionalEncoding, MultiHeadAttention, Dropout, GlobalAveragePooling1D
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 
-# Function to fetch film script data from a URL
-def fetch_script_data(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    paragraphs = soup.find_all('p')  # Adjust based on the HTML structure of the script
-    script_text = ' '.join([p.get_text() for p in paragraphs])
-    return script_text
-script_urls = [
-    'https://imsdb.com/',
-    'https://moviescriptsandscreenplays.com/',
-]
+# Load OpenSubtitles dataset
+dataset, info = tfds.load('open_subtitles/it', with_info=True, split='train')
 
-film_script_data = [fetch_script_data(url) for url in script_urls]
+# Extract dialogue lines from the dataset
+film_script_data = [example['en'].numpy().decode('utf-8') for example in tfds.as_numpy(dataset.take(5000))]
 
 # Tokenize the script data
 tokenizer = Tokenizer()
@@ -40,26 +30,31 @@ input_sequences = pad_sequences(input_sequences, maxlen=max_sequence_length, pad
 X, y = input_sequences[:, :-1], input_sequences[:, -1]
 y = tf.keras.utils.to_categorical(y, num_classes=total_words)
 
-# attention mechanism
-embedding_dim = 50
-units = 128
+# Define the Transformer model
+embedding_dim = 128
+num_heads = 4
+dff = 512
+num_blocks = 4
 
 input_layer = Input(shape=(max_sequence_length-1,))
-embedding_layer = Embedding(total_words, embedding_dim, input_length=max_sequence_length-1)(input_layer)
-lstm_layer = LSTM(units, return_sequences=True)(embedding_layer)
-attention = Attention()([lstm_layer, lstm_layer])
-context_vector = tf.reduce_sum(attention * lstm_layer, axis=1)
-context_vector = tf.expand_dims(context_vector, axis=1)
-merged = tf.keras.layers.Concatenate(axis=-1)([lstm_layer, context_vector])
-lstm_output = LSTM(units)(merged)
-output_layer = Dense(total_words, activation='softmax')(lstm_output)
-model = Model(inputs=input_layer, outputs=output_layer)
+embedding_layer = Embedding(total_words, embedding_dim)(input_layer)
+positional_encoding_layer = PositionalEncoding(max_sequence_length-1, embedding_dim)(embedding_layer)
 
-# Compile the model
+x = positional_encoding_layer
+for _ in range(num_blocks):
+    attn_output = MultiHeadAttention(num_heads=num_heads, key_dim=embedding_dim//num_heads)([x, x, x])
+    attn_output = Dropout(0.1)(attn_output)
+    x = tf.keras.layers.Add()([x, attn_output])
+    x = tf.keras.layers.LayerNormalization(epsilon=1e-6)(x)
+
+x = GlobalAveragePooling1D()(x)
+output_layer = Dense(total_words, activation='softmax')(x)
+
+model = Model(inputs=input_layer, outputs=output_layer)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Train the model
-model.fit(X, y, epochs=50, verbose=1)
+model.fit(X, y, epochs=10, verbose=1)
 
 # Function to generate AI-powered dialogue with attention
 def generate_dialogue_with_attention(seed_text, next_words, model, max_sequence_length, tokenizer):
@@ -78,4 +73,3 @@ def generate_dialogue_with_attention(seed_text, next_words, model, max_sequence_
 # Generate dialogue using the trained model with attention
 generated_dialogue = generate_dialogue_with_attention("JANE", 10, model, max_sequence_length, tokenizer)
 print("Generated Dialogue with Attention:", generated_dialogue)
-
